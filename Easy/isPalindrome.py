448ad73c9bcbe1194b42b06e38ae8a3203462c46
SHEFFmachine <sheff@sheffmachine.com>
2018-01-07 11:39:22 -0800

    Another Solution

--- Easy/isPalindrome.py
+++ Easy/isPalindrome.py
@@ -5,19 +5,35 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        multiplier = 10
         reversed_int = 0
-        multiplier = 10.0
         if x < 0:
             return False
         if x < 10:
             return True
-        while math.floor(x / multiplier) > 0:
-            reversed_int = reversed_int * 10 + \
-                           math.floor((x - math.floor(x / multiplier) * multiplier) / (multiplier / 10))
+        while math.floor(x / multiplier) >= 0:
+            reversed_int = reversed_int * 10 + math.floor(math.floor((x % multiplier)) / multiplier * 10)
+            if x == reversed_int:
+                return True
             multiplier *= 10
-        reversed_int = reversed_int * 10 + math.floor(x / (multiplier / 10))
-        return x - reversed_int == 0.0
+
+        return x == reversed_int
+
+
+
+        # reversed_int = 0
+        # multiplier = 10.0
+        # if x < 0:
+        #     return False
+        # if x < 10:
+        #     return True
+        # while math.floor(x / multiplier) > 0:
+        #     reversed_int = reversed_int * 10 + \
+        #                    math.floor((x - math.floor(x / multiplier) * multiplier) / (multiplier / 10))
+        #     multiplier *= 10
+        # reversed_int = reversed_int * 10 + math.floor(x / (multiplier / 10))
+        # return x - reversed_int == 0.0
 
 
 solution = Solution()
-print(solution.isPalindrome(-2442))
+print(solution.isPalindrome(9999))
