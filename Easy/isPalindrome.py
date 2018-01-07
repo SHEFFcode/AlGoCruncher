6abd156d245f9111e7de5b3b9ffc1aa909bc2f96
SHEFFmachine <sheff@sheffmachine.com>
2018-01-07 11:07:40 -0800

    number pallindrome in O(1) space

---
+++ Easy/isPalindrome.py
@@ -0,0 +1,23 @@
+import math
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        reversed_int = 0
+        multiplier = 10.0
+        if x < 0:
+            return False
+        if x < 10:
+            return True
+        while math.floor(x / multiplier) > 0:
+            reversed_int = reversed_int * 10 + \
+                           math.floor((x - math.floor(x / multiplier) * multiplier) / (multiplier / 10))
+            multiplier *= 10
+        reversed_int = reversed_int * 10 + math.floor(x / (multiplier / 10))
+        return x - reversed_int == 0.0
+
+
+solution = Solution()
+print(solution.isPalindrome(-2442))
