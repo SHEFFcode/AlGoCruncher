7a0a9eccb4b1c9a1f82237b0bdfd75de4ad3ed1f
SHEFFcode <sheff@sheffmachine.com>
2018-01-09 21:03:32 -0800

    Longest absolute file path complete

--- Medium/LongestAbsoluteFilePath.py
+++ Medium/LongestAbsoluteFilePath.py
@@ -15,24 +15,24 @@ class Solution(object):
                 t_count = string_container[i].count('\t')
                 current_string = current_string + '/' + string_container[i][t_count:]
                 pointer += 1
-                if '.' in string_container[i] and current_string > longest_string:
+                if '.' in string_container[i] and len(current_string) > len(longest_string):
                     longest_string = current_string
                 else:
-                    current_dir.append('/' + string_container[i][t_count:])
+                    current_dir.append('' + string_container[i][t_count:])
             elif string_container[i].count('\t') <= t_count:
                 t_count = string_container[i].count('\t')
                 t_level = string_container[i].count('\t')
                 current_dir = current_dir[0:t_level]
                 pointer -= string_container[i].count('\t')
                 if t_count == t_level and '.' in string_container[i]:
-                    current_string = '/'.join(current_dir[:-1]) + '/' + string_container[i][t_count:]
-                    if current_string > longest_string:
+                    current_string = '/'.join(current_dir) + '/' + string_container[i][t_count:]
+                    if len(current_string) > len(longest_string):
                         longest_string = current_string
+                        current_string = '/'.join(current_dir)
                 else:
                     current_string = '/'.join(current_dir) + '/' + string_container[i][t_count:]
-                y = 0
-            y = 0
-        if current_string > longest_string and '.' in current_string:
+                    current_dir.append('' + string_container[i][t_count:])
+        if len(current_string) > len(longest_string) and '.' in current_string:
             if current_string[0:1] == '/':
                 current_string = current_string[1:]
             return len(current_string)
@@ -40,4 +40,4 @@ class Solution(object):
             longest_string = longest_string[1:]
         return len(longest_string)
 solution = Solution()
-print(solution.lengthLongestPath("dir\n    file.txt"))
+print(solution.lengthLongestPath("skd\n\talskjv\n\t\tlskjf\n\t\t\tklsj.slkj\n\t\tsdlfkj.sdlkjf\n\t\tslkdjf.sdfkj\n\tsldkjf\n\t\tlskdjf\n\t\t\tslkdjf.sldkjf\n\t\t\tslkjf\n\t\t\tsfdklj\n\t\t\tlskjdflk.sdkflj\n\t\t\tsdlkjfl\n\t\t\t\tlskdjf\n\t\t\t\t\tlskdjf.sdlkfj\n\t\t\t\t\tlsdkjf\n\t\t\t\t\t\tsldkfjl.sdlfkj\n\t\t\t\tsldfjlkjd\n\t\t\tsdlfjlk\n\t\t\tlsdkjf\n\t\tlsdkjfl\n\tskdjfl\n\t\tsladkfjlj\n\t\tlskjdflkjsdlfjsldjfljslkjlkjslkjslfjlskjgldfjlkfdjbljdbkjdlkjkasljfklasjdfkljaklwejrkljewkljfslkjflksjfvsafjlgjfljgklsdf.a"))
