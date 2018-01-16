f437f1b967a5997550efd83bed1ec04e546ca106
SHEFFmachine <sheff@sheffmachine.com>
2018-01-15 18:39:49 -0800

    Longest value univalue path

--- GO_ogle/LongestUnivaluePath.py
+++ GO_ogle/LongestUnivaluePath.py
@@ -7,61 +7,25 @@
 
 
 class Solution(object):
-    longest = 0
-    current_longest = 0
-    value_to_match = None
+    longest = 0  # class variable to ensure we can update it anywhere in the recursion tree
 
     def longestUnivaluePath(self, root, inheritance=0):
         """
         :type root: TreeNode
         :rtype: int
         """
-        if root is None:
-            return self.current_longest
-        if root.left is None and root.right is None:
-            return self.current_longest
-        current_node = root
-        current_value = root.val
-        if self.value_to_match is None:
-            self.value_to_match = current_value
-        right_child_value = current_node.right.val if current_node.right is not None else False
-        left_child_value = current_node.left.val if current_node.left is not None else False
-        if (right_child_value and left_child_value and self.value_to_match != right_child_value
-                and self.value_to_match != left_child_value):
-            self.current_longest = 0
-            inheritance = 0
-            print("pvalue: {0}, rvalue: {1}, lvalue:{2}".format(self.value_to_match, right_child_value, left_child_value))
-            print("Neither matched, {0}".format(self.current_longest))
-            self.value_to_match = current_node.left.val
-            self.longestUnivaluePath(current_node.left, inheritance)
-            self.current_longest = 0
-            self.value_to_match = current_node.right.val
-            self.longestUnivaluePath(current_node.right, inheritance)
-        elif (left_child_value and right_child_value and self.value_to_match == left_child_value
-              and self.value_to_match == right_child_value):
-            self.value_to_match = current_value
-            self.current_longest += 2
-            print("pvalue: {0}, rvalue: {1}, lvalue:{2}".format(self.value_to_match, right_child_value, left_child_value))
-            print("Both matched, {0}".format(self.current_longest))
-            if self.current_longest > self.longest:
-                self.longest = self.current_longest
-            inheritance += 1
-            self.longestUnivaluePath(current_node.left, inheritance)
-            self.longestUnivaluePath(current_node.right, inheritance)
-        elif left_child_value and self.value_to_match == left_child_value:
-            self.current_longest += 1
-            self.value_to_match = current_value
-            print("pvalue: {0}, rvalue: {1}, lvalue:{2}".format(self.value_to_match, right_child_value, left_child_value))
-            print("left matched, {0}".format(self.current_longest))
-            if self.current_longest > self.longest:
-                self.longest = self.current_longest
-            self.longestUnivaluePath(current_node.left)
-        elif right_child_value and self.value_to_match == right_child_value:
-            self.current_longest += 1
-            self.value_to_match = current_value
-            print("pvalue: {0}, rvalue: {1}, lvalue:{2}".format(self.value_to_match, right_child_value, left_child_value))
-            print("Right matched, {0}".format(self.current_longest))
-            if self.current_longest > self.longest:
-                self.longest = self.current_longest
-            self.longestUnivaluePath(current_node.right)
-        return self.longest
+        # Recursive function that will do all of the work calculating longest path between like valued nodes
+        def traverse(node):
+            if not node:  # case where we have reached the end of the recursion tree and node.next is  None
+                return 0
+            left_len, right_len = traverse(node.left), traverse(node.right)  # this will give the len of each branch
+            # We need this because we want to calculate the longest path treating each higher node as parent
+            # This is the case where longest path is "hanging" off the current node, instead of extending from it
+            left_len_w_current_node = left_len + 1 if node.left and node.left.val == node.val else 0
+            right_len_w_current_node = right_len + 1 if node.right and node.right.val == node.val else 0
+            self.longest = max(self.longest, left_len_w_current_node + right_len_w_current_node)
+            # we return max of either right or left branch, because we can't include both branches in the parents
+            # node calculation since these would not be "hanging" off the parent, but rather from the current node
+            return max(left_len_w_current_node, right_len_w_current_node)
+        traverse(root)  # just calling the traverse function with the root node
+        return self.longest  # since we have been updating the self.longest val throughout we can return it now
