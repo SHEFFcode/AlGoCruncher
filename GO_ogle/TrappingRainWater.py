6ebfe14a280c0ee4072e9ccb6dcfc36c15f9ab09
SHEFFmachine <sheff@sheffmachine.com>
2018-01-20 11:51:16 -0800

    TrappingRainWater

--- GO_ogle/TrappingRainWater.py
+++ GO_ogle/TrappingRainWater.py
@@ -4,33 +4,23 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        height_stack = [0]
-        accounted_for = []
-        distance = [0]
+        length = len(height)
+        if length < 1:
+            return 0
+        max_height_left = [0 for x in range(0, length)]  # list will contain max height to the left of current item
+        max_height_right = [0 for x in range(0, length)]  # list will contain max height to the right of the citem
         water_trapped = 0
-        for i in range(0, len(height)):
-            if height[i] < 1 and i == 0:
-                continue
-            elif height[i] < height_stack[-1]:
-                distance[-1] += 1
-            elif height[i] >= height_stack[-1] and height_stack[-1] != 0:
-                water_trapped += (height_stack.pop() - accounted_for.pop()) * distance.pop()
-                accounted_for[-1] += 1
-                height_stack.append(height[i])
-                accounted_for.append(0)
-                distance.append(0)
-            elif height[i] >= height_stack[-1] and height_stack[-1] == 0:
-                continue
-            elif height[i] < height_stack[:-1]:
-                height_stack.append(height[i])
-                accounted_for.append(0)
-                distance.append(distance[:-1] + 1)
-            elif height[i] > 0:
-                height_stack.append(height[i])
-                accounted_for.append(0)
-                distance.append(1)
+        max_height_left[0] = height[0]  # we know this is true, because there are no elements to the left
+        for i in range(1, length):  # fill up the left array from el 1 to the end
+            max_height_left[i] = max(max_height_left[i - 1], height[i])  # we want the highest to left from cpoint
+        max_height_right[length - 1] = height[length - 1]  # we know this is true because there are no el to the right
+        for i in range(length - 2, -1, -1):  # fill up the right array from len - 1 to start
+            max_height_right[i] = max(max_height_right[i + 1], height[i])  # we want the highest to the right from cpoint
+        # we now calculate the water based on whether celement is lower then highest to left and right
+        for i in range(0, length):
+            water_trapped += min(max_height_right[i], max_height_left[i]) - height[i]
         return water_trapped
 
 solution = Solution()
-solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+print(solution.trap([]))
 
