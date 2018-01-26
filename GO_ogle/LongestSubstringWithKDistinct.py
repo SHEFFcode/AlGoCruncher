19031b12e8d692e5b770ae74b5955c4b36285912
SHEFFmachine <sheff@sheffmachine.com>
2018-01-25 21:19:15 -0800

    Longest substring with K distinct

--- GO_ogle/LongestSubstringWithKDistinct.py
+++ GO_ogle/LongestSubstringWithKDistinct.py
@@ -5,40 +5,22 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        if k == 0 or not s:
-            return 0
-        string_length = len(s)
-        unique_set = set(s)
-        if unique_set < k:
-            return 0
-        window_start = 0
-        window_end = 0
-        max_window_size = 1
-
-        unique_set.clear()
-        unique_set.add(s[0])
-        current_string = s[0]
-
-        for i in xrange(1, string_length):
-            unique_set.add(s[i])
-            window_end += 1
-            current_string += s[i]
-
-            set_length = len(set(current_string))
-
-            while set_length > k:
-                current_string = current_string[1:]
-                set_length = len(set(current_string))
-                window_start += 1
-
-            if window_end - window_start + 1 > max_window_size:
-                max_window_size = window_end - window_start + 1
-        return max_window_size
+        character_dictionary = {}
+        lowest_index_char_in_dict, longest_substring = 0, 0
+        for i, c in enumerate(s):
+            character_dictionary[c] = i
+            if len(character_dictionary) > k:
+                lowest_index_char_in_dict = min(character_dictionary.values())
+                del character_dictionary[s[lowest_index_char_in_dict]]
+                lowest_index_char_in_dict += 1
+            longest_substring = max(i - lowest_index_char_in_dict + 1, longest_substring)
+        return longest_substring
 
 
 
 solution = Solution()
-# print(solution.lengthOfLongestSubstringKDistinct("eceba", 2))
+# print(solution.lengthOfLongestSubstringKDistinct("a@b$5!a8alskj234jasdf*()@$&%&#FJAvjjdaurNNMa8ASDF-0321jf?>{}L:fh", 10))
+# print(solution.lengthOfLongestSubstringKDistinct("aabcedbaba", 2))
 print(solution.lengthOfLongestSubstringKDistinct("ecebaaaajdhflhalrnjaerbasjjjjjjjjjjjjjlladsllllwliejejejslsrhladfzrhljgfbgafnlhagsdblkagfe", 2))
 
 
