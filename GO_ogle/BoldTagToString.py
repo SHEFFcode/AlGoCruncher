6d6242c8e952812c21dc58eada5eb0ff313b7c77
SHEFFmachine <sheff@sheffmachine.com>
2018-02-01 19:36:54 -0800

    Bold tag to string fails

--- GO_ogle/BoldTagToString.py
+++ GO_ogle/BoldTagToString.py
@@ -21,21 +21,26 @@ class Solution(object):
         string_list, single_string = [], ''
         print_dictionary = {}
         for i in range(0, len(s)):
+            if len(single_string) == len(s):
+                break
             if i in dict:
                 single_string += s[i]
                 print_dictionary[i] = i
-                continue
+            if single_string + s[i] not in dict and len(single_string) > 0:
+                print_dictionary[i - 1] = single_string
+                single_string = ''
+                end = max(i + 1, end)
             for j in range(i + 1, len(s) + 1):
-                if s[i:j] in dict:
+                if s[i:j] in dict or (s[i:j] == (j - i) * s[i] and s[i] in dict):
                     if i <= end + 1:
-                        single_string += s[start:j]
-                        end = end + j
-                        start = i
+                        single_string += s[end:j]
+                        end = j
                     else:
                         string_list.append(single_string)
                         print_dictionary[start] = single_string
                         start = i
                         single_string = s[start:j]
+                        end = j
         if len(single_string) > 0:
             print_dictionary[start] = single_string
         bolded_string = self.bold_it_up(s, print_dictionary)
@@ -44,5 +49,6 @@ class Solution(object):
 
 
 solution = Solution()
-print(solution.addBoldTag("abcxyz123", ["abc","123"]))
+# print(solution.addBoldTag("aaabbcc", ["aaa","aab","bc"]))
+print(solution.addBoldTag("aaabbcc",["a","c"]))
 
