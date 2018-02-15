2506f5631139b129445cab979bee548830741240
SHEFFmachine <sheff@sheffmachine.com>
2018-02-14 20:48:25 -0800

    Finally one edit distance

--- GO_ogle/OneEditDistance.py
+++ GO_ogle/OneEditDistance.py
@@ -5,35 +5,28 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) > len(t):
-            first_string, second_string = s, t
-        else:
-            first_string, second_string = t, s
-
-        number_of_edits, column_edits = 0, 0
-        if (len(first_string) == len(second_string) and first_string == second_string) or abs(len(first_string)
-                                                                                              - len(second_string)) > 1:
+        if not s and not t:
             return False
-
-        diff_matrix = [[0 for _ in range(len(second_string))] for _ in range(len(first_string))]
-
-        for i in range(len(first_string)):
-            row_sum = 0
-            for j in range(len(second_string)):
-                if first_string[i] == second_string[j]:
-                    diff_matrix[i][j] = 1
-                    row_sum += 1
-            if row_sum == 0:
+        s_length, t_length = len(s), len(t)
+        if abs(s_length - t_length) > 1:
+            return False
+        i, j, number_of_edits = 0, 0, 0
+        while i < s_length and j < t_length:
+            if s[i] == t[j]:
+                i, j = i + 1, j + 1
+            else:
                 number_of_edits += 1
-        for k in range(len(second_string)):
-            column_sum = 0
-            for l in range(len(first_string)):
-                if diff_matrix[l][k] == 1:
-                    column_sum += 1
-            if column_sum == 0:
-                column_edits += 1
-                number_of_edits = max(number_of_edits, column_edits)
+                if number_of_edits > 1:
+                    return False
+                if s_length > t_length:
+                    i += 1
+                elif t_length > s_length:
+                    j += 1
+                else:
+                    i, j = i + 1, j + 1
+        if i < s_length or j < t_length:
+            number_of_edits += 1
         return number_of_edits == 1
 
 solution = Solution()
-print(solution.isOneEditDistance("geek", "geeks"))
+print(solution.isOneEditDistance("a", ""))
