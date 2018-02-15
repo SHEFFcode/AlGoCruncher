8ffde5cf546e0c3c4e050a32ea196fa7bba3ae87
SHEFFmachine <sheff@sheffmachine.com>
2018-02-15 11:40:12 -0800

    validparens

--- GO_ogle/ValidParens.py
+++ GO_ogle/ValidParens.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        if len(s) % 2 != 0:
+            return False
+        matching_pair = {')': '(', ']': '[', '}': '{'}
+        stack = []
 
+        for char in s:
+            if char not in matching_pair:
+                stack.append(char)
+            else:
+                if stack and matching_pair[char] == stack.pop():
+                    continue
+                else:
+                    return False
+        return True if not stack else False
+
+solution = Solution()
+print(solution.isValid("(("))
