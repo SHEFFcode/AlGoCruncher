45ac8b228852e5258c824e9f57b27961a46ce8ee
SHEFFmachine <sheff@sheffmachine.com>
2018-02-15 20:05:31 -0800

    Get started on the image smoother

---
+++ GO_ogle/ImageSmoother.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def imageSmoother(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: List[List[int]]
+        """
+
+
+solution = Solution()
+solution.get_adjacent_cells([[1, 1, 1], [1, 0, 1], [1, 1, 1]], 0, 0)
