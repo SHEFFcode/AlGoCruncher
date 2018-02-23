c67cbc9b298606c5ac18fb7123f37a666138aef9
SHEFFmachine <sheff@sheffmachine.com>
2018-02-22 16:11:21 -0800

    Max consecutive ones

--- GO_ogle/MaxConsecutiveOnesII.py
+++ GO_ogle/MaxConsecutiveOnesII.py
@@ -4,21 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        post_reset_vals, pre_reset_vals, maximum = 0, 0, 0
-        for digit in nums:
+        if not nums:
+            return None
+        max_1s, current_1s = 0, 0
+        flip_index = None
+        for index, digit in enumerate(nums):
             if digit == 1:
-                pre_reset_vals += 1
-            elif post_reset_vals == 0:
-                post_reset_vals += 1
-            else:
-                c_max = pre_reset_vals + post_reset_vals
-                if c_max > maximum:
-                    maximum = c_max
-                pre_reset_vals = post_reset_vals - 1
-                post_reset_vals = 0
-        if (pre_reset_vals + post_reset_vals) > maximum:
-            maximum = pre_reset_vals + post_reset_vals
-        return maximum
+                current_1s += 1
+            elif digit == 0 and flip_index is None:
+                flip_index = index
+                current_1s += 1  # this is the first time we run into a 0, so we just flip it
+            elif digit == 0 and flip_index is not None:
+                if current_1s > max_1s:
+                    max_1s = current_1s
+                current_1s = index - flip_index
+                flip_index = index
+        if current_1s > max_1s:
+            max_1s = current_1s
+        return max_1s
+
 
 solution = Solution()
-print(solution.findMaxConsecutiveOnes([1,0,1,1,0]))
+print(solution.findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1, 1, 1, 0, 1]))
