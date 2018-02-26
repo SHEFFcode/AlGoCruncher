4a2ccedcc617a212bd948fa9512eaedea1c2da9f
SHEFFmachine <sheff@sheffmachine.com>
2018-02-26 15:29:03 -0800

    Merge sorted arrays

--- GO_ogle/MergeKSortedLists.py
+++ GO_ogle/MergeKSortedLists.py
@@ -10,29 +10,47 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        funnel = [None for node in lists]
-        merged_pointer = None
-        head_nodes = [node for node in lists]
-        next_list_id = [-1]  # we make it into array so we can pass it around by reference
+        if not lists:
+            return lists
+        last = len(lists) - 1
+        while last != 0:
+            i, j = 0, last
+            while i < j:
+                lists[i] = self.mergeTwoLists(lists[i], lists[j])
+                i, j = i + 1, j - 1
+                if i >= j:
+                    last = j
+        return lists[0]
+
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        start_node = ListNode(0)
+        tail_node = start_node
         while True:
-            if not funnel[0]:
-                break
-            if not merged_pointer:
-                merged_pointer = funnel[0]
-                funnel[0] = None
-                self.populate_funnel(funnel, head_nodes, next_list_id)
-            else:
-                merged_pointer.next = funnel[0]
-                funnel[0] = None
-                self.populate_funnel(funnel, head_nodes, next_list_id)
-        merged_pointer.next = None  # this terminates the new merged list
-        return merged_pointer
-
-    def populate_funnel(self, funnel, head_nodes, next_list_id):
-        for index, value in enumerate(head_nodes):
-            if next_list_id == -1:
-                for i in range(len(head_nodes)):
-                    pass
-
-solution = Solution()
-solution.mergeKLists([1, 2, 3])
+            if not l1:
+                if not start_node:
+                    start_node = l2
+                tail_node.next = l2
+                break  # here we have finished with l1 list, and are just appending the l2 node.
+            elif not l2:
+                if not start_node:
+                    start_node = l1
+                tail_node.next = l1
+                break  # here we have finished with the l2 list, and are just appending the l1 list
+            elif l1.val <= l2.val:  # l1 val is smaller than l2, and should go on the end of our list
+                if not start_node:
+                    start_node = l1
+                tail_node.next = l1
+                l1 = l1.next  # move forward the l1 pointer
+                tail_node = tail_node.next  # move forward the tail pointer
+            elif l1.val > l2.val:
+                if not start_node:
+                    start_node = l2
+                tail_node.next = l2
+                l2 = l2.next  # move forward the l2 pointer
+                tail_node = tail_node.next  # move forward the tail pointer
+        return start_node.next
