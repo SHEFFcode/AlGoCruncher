a5e629b3f1eaed62ae4563f50393cf8583c8f449
SHEFFmachine <sheff@sheffmachine.com>
2018-02-27 13:11:28 -0800

    Falling blocks figured out

--- GO_ogle/FallingBlocks.py
+++ GO_ogle/FallingBlocks.py
@@ -1,21 +1,26 @@
-class Solution(object):
+class Solution:
     def fallingSquares(self, positions):
         """
         :type positions: List[List[int]]
         :rtype: List[int]
         """
-        board = dict()  # the board that will contain left edge information for heights
-        absolute_max = list()  # this is the output list, will contain the absolute max for each iteration
-        for index, position in enumerate(positions):  # enumerate through positions
-            current_max = 0
-            for i in range(position[0], position[0] + position[1]):  # check for every spot in the index matrix
-                if i in board:
-                    current_max = board[i] if board[i] > current_max else current_max
-            for i in range(position[0], position[0] + position[1]):
-                board[i] = current_max + position[1]
-            absolute_max_so_far = absolute_max[-1] if absolute_max else 0
-            absolute_max.append(max(current_max + position[1], absolute_max_so_far))
+        absolute_max = list()  # contains absolute max at every iteration
+        board = dict()  # contains a sparse representation of the board
+        for start_corner, side_length in positions:
+            left, right = start_corner, start_corner + side_length - 1  # we want to do -1 because corners don't 'stick'
+            # compare to see if this block overlaps with L/R boundaries of existing blocks
+            nearby = [key for key in board.keys() if not (key[1] < left or key[0] > right)]
+            # finds height of block based on board of existing and overlapping blocks
+            if len(nearby) > 0:
+                current_height = max(board[key] for key in nearby) + side_length  # height so far, + height of c_block
+            else:
+                current_height = side_length
+            # update the board for left and right boundaries
+            board[(left, right)] = current_height
+            # add height to absolute_max
+            max_height_so_far = absolute_max[-1] if absolute_max else 0
+            absolute_max.append(max(current_height, max_height_so_far))
         return absolute_max
 
 solution = Solution()
-print(solution.fallingSquares([[4,6],[4,2],[4,3]]))
+print(solution.fallingSquares([[4, 6], [4, 2], [4, 3]]))
