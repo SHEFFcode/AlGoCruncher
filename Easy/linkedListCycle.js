b54debaaecefd63c06c4cc9bcaf6dbf875f0792e
SHEFFmachine <sheff@sheffmachine.com>
2018-03-15 09:27:55 -0700

    linked list cycle

---
+++ Easy/linkedListCycle.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let slow = head, 
+        fast = head;
+
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (slow.val === fast.val) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+/*
+ G: Linked List
+ O: Boolean does it have a cycle in it
+ T: Any
+ S: O(1)
+
+1 => 2 => 3 => 4 => 5 => 4
+                    *^
+
+
+ */
