25b7daa944be9e37af8cb539ae657792e1f8537f
SHEFFmachine <sheff@sheffmachine.com>
2018-03-22 08:48:19 -0700

    small change

--- Easy/mergeTwoTrees.js
+++ Easy/mergeTwoTrees.js
@@ -14,7 +14,7 @@ var mergeTrees = function(t1, t2) {
   if (!t1 && !t2) {
       return null;
   }
-  let newNode = new TreeNode((t1 ? t1.val : 0) + (t2 ? t2.val : 0);
+  let newNode = new TreeNode((t1 ? t1.val : 0) + (t2 ? t2.val : 0));
   newNode.left = mergeTrees(t1 && t1.left, t2 && t2.left);
   newNode.right = mergeTrees(t1 && t1.right, t2 && t2.right);
   return newNode;
