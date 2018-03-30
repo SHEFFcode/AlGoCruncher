53042ed684f6665e5c946bf9bb1486119da5c1ae
SHEFFmachine <sheff@sheffmachine.com>
2018-03-29 17:40:30 -0700

    start working on largest BST subtree

--- GO_ogle/largestBSTSubtree.js
+++ GO_ogle/largestBSTSubtree.js
@@ -1,3 +1,6 @@
+
+
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -9,29 +12,34 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var largestBSTSubtree = function(root) {
-    return _traverse(root)[1];
+var largestBSTSubtree = function (root) {
+  let result = _traverse(root);
+  if (result[4] === 1) {
+    return result[1] + 1;
+  }
+  return result[1];
 };
 
-function _traverse(cNode) {
+function _traverse(cNode, depth = 0) {
   if (!cNode) {
-    return [null, 0, 0, 0];
+    return [null, 0, 0, 0, depth];
   }
 
-  let leftResult = _traverse(cNode.left);
-  let rightResult = _traverse(cNode.right);
+  let leftResult = _traverse(cNode.left, depth + 1);
+  let rightResult = _traverse(cNode.right, depth + 1);
 
-    
-    if (leftResult[0] === null && rightResult[0] === null) {
-        return [true, 1, cNode.val, cNode.val];
-    } else if (!leftResult[0] || !rightResult[0]) {
-    return [false, Math.max(leftResult[1], rightResult[1]), 0, 0];
+  console.log(leftResult);
+  console.log(rightResult);
+
+  if (leftResult[0] === null && rightResult[0] === null) {
+    return [true, 1, cNode.val, cNode.val, depth];
+  } else if (leftResult[0] === false || rightResult[0] === false) {
+    return [false, Math.max(leftResult[1], rightResult[1]), 0, 0, Math.max(leftResult[4], rightResult[4])];
   } else {
     if (rightResult[2] > cNode.val && leftResult[3] < cNode.val) {
-        
-      return [true, rightResult[1] + leftResult[1] + 1, leftResult[3], rightResult[2]];
+      return [true, rightResult[1] + leftResult[1], leftResult[3], rightResult[2], Math.max(leftResult[4], rightResult[4])];
     } else {
-      return [false, Math.max(rightResult[1], leftResult[1]), 0, 0];
+      return [false, Math.max(rightResult[1], leftResult[1]), 0, 0, Math.max(leftResult[4], rightResult[4])];
     }
   }
 }
