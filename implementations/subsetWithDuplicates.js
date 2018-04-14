8d4116fc8e5cd9405ffcb5f264cfb2e59b37a90a
SHEFFmachine <sheff@sheffmachine.com>
2018-04-14 13:24:36 -0700

    subset

---
+++ implementations/subsetWithDuplicates.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  nums.sort((a, b) => a - b);
+  let brain = {};
+  var result = [[]];
+
+  for (var i = 0; i < array.length; i++) {
+    //this line is crucial! It prevents us from infinite loop
+    var len = result.length;
+    for (var x = 0; x < len; x++) {
+        let key = result[x].concat(array[i]);
+        if (!brain.hasOwnProperty(key)) {
+            result.push(result[x].concat(array[i]))
+            brain[key] = true;
+        }
+    }
+  }
+
+  return result;
+};
