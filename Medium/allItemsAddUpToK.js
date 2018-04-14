8e82fc5728c3a2d15199ed4e8be9b1f09d8a7d16
SHEFFmachine <sheff@sheffmachine.com>
2018-04-14 16:53:17 -0700

    allitems add up to k

---
+++ Medium/allItemsAddUpToK.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    let result = [];
+    let brain = {};
+    traverse(candidates, target, result, null, [], brain);
+    return result;
+};
+
+function traverse(arr, k, result, item, temp, brain) {
+  if (k === 0) {
+    let sortedTemp = temp.slice().sort((a, b) => a - b);
+    let key = JSON.stringify(sortedTemp);
+    if (!brain.hasOwnProperty(key)) {
+      result.push(sortedTemp);
+      brain[key] = true;
+    }
+    return;
+  } else if (k < 0) {
+    temp.pop();
+    return;
+  }
+
+  if (item !== null) {
+    k -= item;
+    if (k > -1)
+      temp.push(item);
+  }
+  arr.forEach((number) => {
+    traverse(arr, k, result, number, temp.slice(), brain);
+  });
+}
+
+combinationSum([2,3,6,7], 7);
