636e14ca60aa35e3215c0f77b0aa5e2aeb532bd1
SHEFFmachine <sheff@sheffmachine.com>
2018-04-15 11:05:23 -0700

    Move count primes to implementations

---
+++ implementations/countPrimes.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+  let allNumbers = Array(n + 1).fill(1);
+  let count = 0;
+
+  allNumbers[0] = 0;
+  allNumbers[1] = 0;
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (allNumbers[i] === 1) { // if a number is prime, all its multiples are not
+      for (let j = 2; i * j <= n; j++) {
+        allNumbers[i * j] = 0;
+      }
+    }
+  }
+
+  allNumbers.forEach((number) => {
+    if (number === 1)
+      count++;
+  });
+  return count;
+};
+
+countPrimes(2);
