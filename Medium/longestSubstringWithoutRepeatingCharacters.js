2c6fa49f668c3cf6c86990cd1aad2db0917b310e
yuriy-shefer-ck <yuriy.shefer@creditkarma.com>
2018-11-18 17:14:42 -0800

    Fastest way to solve it

--- Medium/longestSubstringWithoutRepeatingCharacters.js
+++ Medium/longestSubstringWithoutRepeatingCharacters.js
@@ -23,3 +23,30 @@ var lengthOfLongestSubstring = function (s) {
     }
     return longestSubstringNoRepeat
 };
+
+/*
+    Fast Solution
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let len = s.length;
+    if (len === 0 || len === 1) return len;
+    let longest = 0;
+    let start = 0;
+
+    for (let end = 0; end < s.length; end++) {
+        const curr = s[end];
+        const index = s.indexOf(curr, start);
+        if (index >= start && index < end) {
+            start = index + 1;
+        } else {
+            longest = Math.max(longest, end - start);
+        }
+    }
+
+    return longest + 1;
+};
