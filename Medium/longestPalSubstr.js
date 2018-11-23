873c64fd7b29f223cd8de1488fa85489862f7623
SHEFFmachine <sheff@sheffmachine.com>
2018-11-22 17:12:05 -0800

    Longest pallindrome alog

---
+++ Medium/longestPalSubstr.js
@@ -0,0 +1,85 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  const len = s.length
+  if (len < 2) {
+    return s
+  }
+  const table = createTable(len)
+  let palindromeStartStopTuple = [0, 1]
+  for (let i = 0; i < len; i++) {
+    table[i][i] = true
+  }
+
+  // check for sub-string of length 2
+  for (let i = 0; i < len - 1; i++) {
+    if (s[i] === s[i + 1]) {
+      table[i][i + 1] = true
+      start = i
+      palindromeStartStopTuple = [i, i + 1 + 1]
+    }
+  }
+
+  // k is the length of the substring. This checks for lengths greater then 2
+  for (let k = 3; k <= len; k++) {
+    for (let i = 0; i < len - k + 1; i++) {
+      let j = i + k - 1 // is the ending index of a string starting at i of length k
+      if (table[i + 1][j - 1] && s[i] === s[j]) {
+        table[i][j] = true
+        if (k > palindromeStartStopTuple[1] - palindromeStartStopTuple[0]) {
+          palindromeStartStopTuple = [i, j + 1]
+        }
+      }
+    }
+  }
+
+  return s.substring(palindromeStartStopTuple[0], palindromeStartStopTuple[1])
+}
+
+function createTable(len) {
+  return new Array(len)
+    .fill(undefined)
+    .map(() => new Array(len).fill(undefined))
+}
+
+const result = longestPalindrome('ccc')
+console.log(result)
+
+// Other Solution
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome = str => {
+  let longest = ''
+
+  for (let i = 0; i < str.length; i++) {
+    let head = i
+    let tail
+    let count
+
+    while (str[i] === str[i + 1]) {
+      i++
+    }
+
+    tail = i
+    count = tail - head + 1
+
+    for (
+      let offset = 1;
+      head - offset > -1 && tail + offset < str.length;
+      offset++
+    ) {
+      if (str[head - offset] !== str[tail + offset]) break
+      count += 2
+    }
+
+    if (count >= longest.length) {
+      longest = str.substr(head - (count - (tail - head + 1)) / 2, count)
+    }
+  }
+
+  return longest
+}
