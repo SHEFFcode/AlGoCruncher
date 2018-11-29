8457b4d335fd5723c4b454fea031af7c51eb02a7
SHEFFmachine <sheff@sheffmachine.com>
2018-11-28 22:20:37 -0800

    Peak mountain in an array

---
+++ Easy/peakMountainInArray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(A) {
+  let index
+  A.reduce((accumulator, currentValue, currentIndex) => {
+    if (currentValue > accumulator) {
+      accumulator = currentValue
+      index = currentIndex
+      return accumulator
+    }
+    return accumulator
+  })
+
+  return index
+}
