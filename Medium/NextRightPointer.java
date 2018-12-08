9ec58f375d3d78abcb95096cbc7b33b740f8f492
SHEFFmachine <sheff@sheffmachine.com>
2018-12-07 19:16:43 -0800

    NextRightPointer

---
+++ Medium/NextRightPointer.java
@@ -0,0 +1,26 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Definition for binary tree with next pointer. public class TreeLinkNode { int
+ * val; TreeLinkNode left, right, next; TreeLinkNode(int x) { val = x; } }
+ */
+public class Solution {
+  public void connect(TreeLinkNode root) {
+    TreeLinkNode levelStart = root;
+    while (levelStart != null) {
+      TreeLinkNode cNode = levelStart;
+      while (cNode != null) {
+        if (cNode.left != null) {
+          cNode.left.next = cNode.right;
+        }
+        if (cNode.right != null && cNode.next != null) {
+          cNode.right.next = cNode.next.left;
+        }
+
+        cNode = cNode.next;
+      }
+      levelStart = levelStart.left;
+    }
+  }
+}
