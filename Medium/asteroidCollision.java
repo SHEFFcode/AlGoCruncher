98a232719298069be508bbd14f995ac580e4afbf
yuriy-shefer-ck <yuriy.shefer@creditkarma.com>
2018-12-11 22:22:55 -0800

    Get pretty close to the solution

--- Medium/asteroidCollision.java
+++ Medium/asteroidCollision.java
@@ -1,14 +1,51 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+
         if (asteroids.length < 2) {
             return asteroids;
         }
-        
-        for (int i = 0, j = 0; i < asteroids.length && j < asteroids.length; i++, j++) {
-            if (asteroids[i] < 0) {
+
+        int[] newAsteroids = asteroids;
+
+        for (int i = 0, j = i + 1; i < asteroids.length && j < asteroids.length; i++, j++) {
+            if (asteroids[i] <= 0) {
                 continue;
             }
-            j = i + 1;
+            if (asteroids[j] < 0) {
+                if (Math.abs(asteroids[i]) != Math.abs(asteroids[j])) {
+                    int winner = Math.abs(asteroids[i]) > Math.abs(asteroids[j]) ? asteroids[i] : asteroids[j];
+                    newAsteroids = new int[asteroids.length - 1];
+                    for (int start = 0; start < newAsteroids.length; start++) {
+                        if (start == i) {
+                            System.out.println("I made it here");
+                            System.out.println(winner);
+                            System.out.println("Start is ");
+                            System.out.println(i);
+                            newAsteroids[start] = winner;
+                        } else {
+                            newAsteroids[start] = asteroids[start];
+                        }
+                    }
+                    System.out.println(Arrays.toString(newAsteroids));
+
+                    return asteroidCollision(newAsteroids);
+
+                } else {
+                    newAsteroids = new int[asteroids.length - 2];
+                    for (int start = 0; start < newAsteroids.length; start++) {
+                        if (start == i || start == j) {
+                            continue;
+                        }
+                        newAsteroids[start] = asteroids[start];
+                    }
+                    System.out.println(Arrays.toString(newAsteroids));
+
+                    return asteroidCollision(newAsteroids);
+
+                }
+            }
         }
+        System.out.println(Arrays.toString(newAsteroids));
+        return newAsteroids;
     }
 }
