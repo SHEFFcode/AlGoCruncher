8ce6d46b4e8da68710a4bc35b3b93b9c3ce4b5e8
SHEFFmachine <sheff@sheffmachine.com>
2018-12-21 11:57:03 -0500

    Solution with explanation

--- Easy/sqrt_x.scala
+++ Easy/sqrt_x.scala
@@ -2,11 +2,32 @@ object Solution {
     def mySqrt(x: Int): Int = {
         if (x == 0 || x == 1) return x
 
-        def calc(temp: Int): Int = {
-            if (temp <= (temp + x / temp) / 2) return temp
-            else calc(Math.abs((temp + x/temp)/2))
+        def calc(mid: Int): Int = {
+            if (mid <= (mid + x / mid) / 2) return mid
+            else calc(Math.abs((mid + x / mid) / 2))
         }
 
         calc(x)
     }
 }
+
+/*
+input: 8
+
+if (8 <= (8 + 8/8) / 2) => (8 < (8 + 1) / 2) => (8 < 9 / 2) => 8 < 4
+if (4 <= (4 + 8 / 4) / 2) => (4 <= (4 + 2) / 2) => (4 < 6 / 2) => 4 < 3
+if (3 <= (3 + (8 / 3) / 2) => (3 < (3 + 2) / 2) => (3 < 5 / 2) => 3 < 2
+if (2 <= (2 + (8 / 2) / 2) => (2 < (2 + 4) / 2) => (2 < 6 / 2) => 2 < 3 => 2
+
+
+(mid + x / mid) / 2
+
+(mid * mid + x) / 2
+
+(mid^2 + x) / 2
+
+mid ^ 2 == x
+
+(mid ^ 2 + mid ^ 2) / 2, which gets us the actual midpoint. So if the mid ^ 2 is less then the expression, we passed the solution
+
+*/
