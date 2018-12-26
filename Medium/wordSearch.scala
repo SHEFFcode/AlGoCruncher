b4bf1bd34145d0f9d89f0b94a04a494103c73d6f
SHEFFmachine <sheff@sheffmachine.com>
2018-12-26 13:02:07 -0800

    Word search scala

--- Medium/wordSearch.scala
+++ Medium/wordSearch.scala
@@ -1,22 +1,27 @@
 object Solution {
     def exist(board: Array[Array[Char]], word: String): Boolean = {
-        for {
-          i <- board
-          j <- board[i]
-        } yield {
-          def recurse(i: Int, j: Int, board: Array[Array[Char]], word: String, index: Int): Option[Boolean] = {
-            if (board(i)(j) == word(index)) {
-              val temp = board(j)(j)
-              board(i)(j) = '.'
-              val newIndex = index + 1
-              val result = recurse(i + 1, j, board, word, newIndex).orElse(false) || recurse(i - 1, j, board, word, newIndex).orElse(false)
-              || recurse(i, j + 1, board, word, newIndex).orElse(false) || recurse(i, j - 1, board, word, newIndex).orElse(false)
-              board(i)(j) = temp
-              result
-            }
-            else false
-          }
-          recurse(i, j, board, word, 0)
-        }
+      for {
+        i <- 0 to board.length
+        j <- 0 to board(0).length
+      } yield {
+        if (exists(board, i, j, word, 0)) return true
+      }
+      false
     }
-}
+
+    def exists(board: Array[Array[Char]], i: Int, j: Int, word: String, wIndex: Int): Boolean = {
+      if (wIndex == word.length) return true
+      if (i < 0 || j < 0 || (i == board.length) || (j == board(i).length)) return false
+      if (board(i)(j) != word(wIndex)) return false
+      val temp = board(i)(j)
+      board(i)(j) = '.'
+      val nextWIndex = wIndex + 1
+      val wordExists =
+        exists(board, i, j + 1, word, nextWIndex) ||
+        exists(board, i, j - 1, word, nextWIndex) ||
+        exists(board, i + 1, j, word, nextWIndex) ||
+        exists(board, i - 1, j, word, nextWIndex)
+      board(i)(j) = temp
+      wordExists
+    }
+  }
