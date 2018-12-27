04648d3c28d2164f43268f94dd0c3fb2cde5d26e
SHEFFmachine <sheff@sheffmachine.com>
2018-12-26 17:36:03 -0800

    Houston, we still have a problem

--- Medium/missingRanges.scala
+++ Medium/missingRanges.scala
@@ -16,7 +16,10 @@ object Solution {
             addRange(nums(i), nums(j), missingRanges)
           } 
         }
-        if (nums(nums.length - 1) != upper) addRange(nums(nums.length - 1), upper + 1, missingRanges)
+        if (nums(nums.length - 1) != upper) {
+          val newTo = if (nums(nums.length - 1) == Integer.MAX_VALUE) nums(nums.length - 1) else nums(nums.length - 1) + 1
+          addRange(nums(nums.length - 1), newTo, missingRanges)
+        } 
         
         if (!missingRanges.isEmpty) missingRanges.toString.split(',').toList else List()
     }
@@ -30,3 +33,60 @@ object Solution {
       } 
     }
 }
+
+/*
+  Correct Solution
+ */
+
+ object Solution {
+    def findMissingRanges(nums: Array[Int], lower: Int, upper: Int): List[String] = {
+      val missingRanges = new StringBuilder
+
+      def recurse(nums: Array[Int], lower: Int, upper: Int, sb: StringBuilder, index: Int): StringBuilder = {
+        val num = nums(index)
+        if (lower < num - 1) sb ++= s"$lower->${num - 1},"
+        else if (lower == num - 1) sb ++= s"$lower,"
+        else if (num == Integer.MAX_VALUE) return sb
+        else if (index == nums.length - 1) {
+          if (lower < upper) sb ++= s"$lower->$upper"
+          if (lower == upper) sb ++= s"$lower"
+          return sb
+        }
+        recurse(nums, num + 1, upper, sb, index + 1)
+      }
+
+      recurse(nums, lower, upper, missingRanges, 0)
+      if (missingRanges.nonEmpty) missingRanges.toString.split(',').toList else List()
+    }
+  }
+
+  /*
+  Another attempt, this needs a fix where we will not add to num before recursion, but will add to it after recursion and after check if its in bounds
+  */
+
+   object Solution {
+    def findMissingRanges(nums: Array[Int], lower: Int, upper: Int): List[String] = {
+      val missingRanges = new StringBuilder
+
+      def recurse(nums: Array[Int], lower: Int, upper: Int, sb: StringBuilder, index: Int): StringBuilder = {
+        if (lower == Integer.MAX_VALUE) return sb
+          if (index == nums.length) {
+              println(lower, upper)
+              if (lower < upper) sb ++= s"$lower->$upper,"
+              if (lower == upper) sb ++= s"$lower,"
+              return sb
+          }
+          val num = nums(index)
+          if (num == Integer.MIN_VALUE) recurse(nums, num, upper, sb, index + 1)
+          
+          if (lower < num - 1) sb ++= s"$lower->${num - 1},"
+          if (lower == num - 1) sb ++= s"$lower,"
+          if (num == Integer.MAX_VALUE) return sb
+          
+          recurse(nums, num, upper, sb, index + 1)
+      }
+
+      recurse(nums, lower, upper, missingRanges, 0)
+      if (missingRanges.nonEmpty) missingRanges.toString.split(',').toList else List()
+    }
+  }
