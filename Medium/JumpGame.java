56bb14302dccf9a0231949d15655f30821c9b4dd
yuriy-shefer-ck <yuriy.shefer@creditkarma.com>
2019-02-03 15:51:45 -0800

    We have a solution to jump game

---
+++ Medium/JumpGame.java
@@ -0,0 +1,25 @@
+/**
+ * The intuition is that if we can jump past the length of the array, we can jump to the end index.
+ * 
+ * Max Reachable = 2, 4, 4, 4, 8
+ * 
+ * [2,3,1,1,4]
+ *          * 
+ */
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int longest = 0; // let's set up the pointers
+        for (int i = 0; i < n; i++) {
+            System.out.println(longest);
+            if (i > longest) { // we want to break here if i is ever bigger then longest, since we got to a point we could not have reached.
+                break;
+            }
+            longest = Math.max(nums[i] + i, longest); // we never reduce the longest we could have jumped
+
+        }
+        System.out.println(longest);
+        return longest >= n - 1;
+    }
+}
