14c4cce166ca6f04822092dc1919de7dd05f8751
SHEFFmachine <sheff@sheffmachine.com>
2019-06-01 22:39:41 -0700

    Remove the log staTements.

--- Medium/subarraySumEqualsK.java
+++ Medium/subarraySumEqualsK.java
@@ -8,15 +8,9 @@ class Solution {
             for (int j = i; j < nums.length; j++) {
                 remainder -= nums[j];
                 if (remainder == 0) {
-                    System.out.println("The number of nums is " + nums[j]);
-                    System.out.println("Count is " + (count + 1));
-                    System.out.println("Remainder is " + (remainder));
                     count++;
                     // if match is found, ++ the count by each subsequent 0
                     while (j + 1 < nums.length && nums[j + 1] == 0) {
-                        System.out.println("The number of nums is " + nums[j + 1]);
-                            System.out.println("J is " + (j + 1));
-                            System.out.println("Count is " + (count + 1));
                         j++;
                         count++;
                     }
