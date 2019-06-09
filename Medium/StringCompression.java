501622ab9eba7c43383e041bcfde263d8b42f745
SHEFFmachine <sheff@sheffmachine.com>
2019-06-08 23:26:40 -0700

    Compress string solution

--- Medium/StringCompression.java
+++ Medium/StringCompression.java
@@ -3,20 +3,27 @@ import java.util.stream.IntStream;
 
 class Solution {
   public int compress(char[] chars) {
-    IntStream charStream = CharBuffer.wrap(chars).chars();
-    StringBuilder compressedString = new StringBuilder();
-    int[] count = { 0 };
-    charStream.reduce((prevChar, currentChar) -> {
-      if (currentChar == prevChar) {
-        count[0]++;
-      } else {
-        compressedString.append("" + (char) prevChar + count[0]);
+    int i = 0; // will keep track of where to put
+
+    for (int j = 0; j < chars.length;) { // we will increment i along with j in the outer loop
+      int count = 1; // we got at least 1 letter.
+      chars[i++] = chars[j++]; // we will do our incrementation here because we want i to start at 0 for empty
+                               // array, but go to 1 if we get into this loop
+
+      while (j < chars.length && chars[j] == chars[j - 1]) {
+        j++; // in the inner loop we only move j, as it will tell us how many letter we will
+             // compress.
+        count++; // increase compressed letter count.
       }
-      return currentChar;
-    });
 
-    System.out.println(compressedString.toString());
+      if (count > 1) { // if we have count greater than 1, we want to update the chars[] in place
+        for (char c : String.valueOf(count).toCharArray()) { // method to iterate through char[] version of count
+          chars[i++] = c;
+        }
+      }
+    }
 
-    return compressedString.length();
+    return i; // this will correspond to the length of compressed string, everything after is
+              // uncompressed remainder.
   }
 }
