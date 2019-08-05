4abb1dd353abd02096a0d4795d34453b21b3ac38
SHEFFmachine <sheff@sheffmachine.com>
2019-08-04 19:33:37 -0700

    Get started on a scala solution

---
+++ Medium/maximumProductSubarray.scala
@@ -0,0 +1,18 @@
+object Solution {
+  def maxProduct(nums: Array[Int]): Int = {
+    if (nums.length < 2) return nums(0)
+
+    val maxMinProd = nums.foldLeft((nums(0), nums(0), nums(0)))((accumulator, current) => {
+      val maxPositive = accumulator._1
+      val maxNegative = accumulator._2
+      val maxProd = accumulator._3
+
+      val newMaxPositive = math.max(math.max(current, maxPositive * current), maxNegative * current)
+      val newMaxNegative = math.min(math.min(current, maxNegative * current), maxPositive * current)
+      val newMaxProd = math.max(newMaxPositive, maxProd)
+      (newMaxPositive, newMaxNegative, newMaxProd)
+    })
+
+    maxMinProd._3
+  }
+}
