c4108872a01f7eb57a8a30372618d2760c1de46e
SHEFFmachine <sheff@sheffmachine.com>
2019-08-11 12:02:32 -0700

    LCS solution works in java

--- GO_ogle/LCS.java
+++ GO_ogle/LCS.java
@@ -1,22 +1,18 @@
 class Solution {
   public int longestCommonSubsequence(String text1, String text2) {
-    char[] one = text1.length() < text2.length() ? text1.toCharArray() : text2.toCharArray();
-    char[] two = text1.length() >= text2.length() ? text1.toCharArray() : text2.toCharArray();
-    int iMax = one.length;
-    int jMax = two.length;
-    int lcs = 0;
-    int jMem = 0;
+    int iMax = text1.length(), jMax = text2.length();
+    char[] one = text1.toCharArray(), two = text2.toCharArray();
+    int[][] cache = new int[iMax + 1][jMax + 1];
 
-    for (int i = 0; i < iMax; i++) {
-      for (int j = jMem; j < jMax; j++) {
-        if (one[i] == two[j]) {
-          lcs++;
-          jMem = j + 1;
-          break;
+    for (int i = 1; i <= iMax; i++) {
+      for (int j = 1; j <= jMax; j++) {
+        if (one[i - 1] == two[j - 1]) {
+          cache[i][j] = cache[i - 1][j - 1] + 1;
+        } else {
+          cache[i][j] = Math.max(cache[i - 1][j], cache[i][j - 1]);
         }
       }
     }
-
-    return lcs;
+    return cache[iMax][jMax];
   }
 }
