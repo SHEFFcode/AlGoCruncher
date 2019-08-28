603dfd12662460a557f55cff2b8a3c60a11f5d52
yuriy-shefer-ck <yuriy.shefer@creditkarma.com>
2019-08-27 20:55:05 -0700

    We have a better solution

--- Hard/SlidingWindowMaximum.java
+++ Hard/SlidingWindowMaximum.java
@@ -1,27 +1,27 @@
 class Solution {
     int[] maxSlidingWindow(int[] nums, int k) {
         if (nums.length == 0) {
-            return new int[0]; // otherwise we will return too large an 
+            return new int[0]; // otherwise we will return too large an
         }
-        ArrayDeque<Integer> window = new ArrayDeque<>(k); // reasonable initial size
+        ArrayDeque<Integer> window = new ArrayDeque<>(k); // reasonable initial size, will hold the indexes of values
         int[] slidingWindowMax = new int[nums.length - k + 1]; // size of our output array
         int slidingWindowIndex = 0;
 
         for (int i = 0; i < nums.length; i++) {
             int current = nums[i];
 
-
-            while ((window.size() > 0 && (window.peek() < current || (i >= k && window.peek() == nums[i - k])))) {
-                window.pollFirst(); // let's remove items
-//                window.offerLast(current);
+            if (!window.isEmpty() && window.peekFirst() == i - k) {
+                window.pollFirst();
             }
 
-            if (window.size() == 0 || current < window.peekFirst()) {
-                window.offerLast(current);
+            while (!window.isEmpty() && nums[window.peekLast()] < current) {
+                window.pollLast();
             }
+
+            window.offerLast(i);
 
             if (i >= k - 1) {
-                slidingWindowMax[slidingWindowIndex++] = window.peekFirst();
+                slidingWindowMax[slidingWindowIndex++] = nums[window.peekFirst()];
             }
         }
 
