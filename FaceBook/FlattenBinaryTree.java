8f1c3f7e4b5fba773ad88e3d6406827d7ca7ac53
SHEFFmachine <sheff@sheffmachine.com>
2019-08-31 15:58:52 -0700

    A better iterative implementation that is easier to read.

--- FaceBook/FlattenBinaryTree.java
+++ FaceBook/FlattenBinaryTree.java
@@ -1,23 +1,29 @@
-import java.util.LinkedList;
-
-/**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
- */
 class Solution {
   public void flatten(TreeNode root) {
-    if (root == null)
+    if (root == null) {
       return;
+    }
 
-    flatten(root.left);
-    flatten(root.right);
-    TreeNode left = root.left;
-    TreeNode right = root.right;
-    root.left = null;
-    root.right = left;
-    while (root.right != null)
-      root = root.right;
-    root.right = right;
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      TreeNode cNode = stack.pop();
+
+      if (cNode.right != null) {
+        stack.push(cNode.right);
+      }
+
+      if (cNode.left != null) {
+        stack.push(cNode.left);
+      }
+
+      if (!stack.isEmpty()) {
+        cNode.right = stack.peek();
+      }
+
+      cNode.left = null;
+    }
   }
 }
 
