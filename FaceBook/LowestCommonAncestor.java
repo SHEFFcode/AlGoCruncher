cb1e96796ee078bff5532df8d68e38a45cd8b848
SHEFFmachine <sheff@sheffmachine.com>
2019-09-01 12:16:15 -0700

    LowestCommon Ancestor.

---
+++ FaceBook/LowestCommonAncestor.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        boolean foundSelf = root.val == p.val || root.val == q.val;
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        boolean foundRight = right != null;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        boolean foundLeft = left != null;
+
+        if (foundLeft && foundRight || ((foundLeft && foundSelf) || (foundRight && foundSelf))) {
+            return root;
+        } else if (foundSelf) {
+            return root;
+        } else if (foundRight) {
+            return right;
+        } else if (foundLeft) {
+            return left;
+        } else {
+            return null;
+        }
+    }
+}
+
+/**
+ * The key to understand here is that we want to treat any tree problem by
+ * looking at a single node. So we basically have some base cases, then
+ * recursive cases, and then a return case.
+ * 
+ * Case 1, find 2, 3: 2 / \ 1 3
+ * 
+ * if (cNode == null) return null; if (cNode.val == p.val || cNode.val == q.val)
+ * selfNode = true bool foundRight = traverse(root.right, p, q) != null bool
+ * foundLeft = traverse(root.left, p, q) != null if ((foundLeft && foundRight)
+ * || (foundLeft && self || foundRight && self)) return root; // this will be
+ * the final answer
+ */
