fa7c39de5dceed16fc7396fc2eb643d7e09e1a32
SHEFFmachine <sheff@sheffmachine.com>
2019-09-07 12:37:03 -0700

    Decode ways

--- FaceBook/DecodeWays.java
+++ FaceBook/DecodeWays.java
@@ -1,29 +1,43 @@
+import java.util.Arrays;
+
 class Solution {
     public int numDecodings(String s) {
-
-        Map<String, Integer> numDecodingsForSuffix = new HashMap<>();
-
-        return decode(s, numDecodingsForSuffix, 0);
+        int[] cache = new int[s.length() + 1]; // we want to account for empty string here, so we will go 1 longer then
+                                               // string
+        Arrays.fill(cache, -1); // we will fill with -1 because that will indicate unexplored values
+        cache[s.length()] = 1; // for empty string we will return 1 by definition
+        return decode(s, 0, cache);
     }
 
-    private int decode(String s, Map<String, Integer> numDecodingsForSuffix, int numDec) {
-        if (s.length() < 2) {
-            return numDec + s.length();
-        } else if (s.length() == 2) {
-            return numDec + Integer.parseInt(s.substring(0, 2)) < 27 ? 2 : 1;
+    private int decode(String s, int index, int[] cache) {
+        /**
+         * If we already have a solution for this index, we will return it from the
+         * cache
+         */
+        if (cache[index] != -1) { // this is both our goal and our base case.
+            return cache[index];
         }
 
-        // String suffix = s.substring(2);
-        // if (numDecodingsForSuffix.containsKey(s)) {
-        // return numDecodingsForSuffix.get(s);
-        // }
+        char cChar = s.charAt(index); // let's take down the current character
+        if (cChar == '0') {
+            /**
+             * there are not ways to decode 0, so all numbers after 0 are disregarded,
+             * unless we had something like a 10 or a 20 where these are valid numbers and
+             * we sort of skip anything that starts with 0. If we have 560231321 it will be
+             * zero, since 56 cannot be a number and 0 cannot begin a sequence
+             */
+            return 0; // this is a constraint
+        }
 
-        int additionalWays = decode(s.substring(2), numDecodingsForSuffix,
-                numDec + Integer.parseInt(s.substring(0, 2)) < 27 ? 2 : 1);
+        int result = decode(s, index + 1, cache); // the result will be decode (k + 1) + decode (k + 2), but only
+                                                  // if k + 2 is within the constraints
+        if (s.length() - index >= 2 && Integer.valueOf(s.substring(index, index + 2)) <= 26) { // this is
+            // another
+            // constraint
+            result += decode(s, index + 2, cache);
+        }
 
-        // numDecodingsForSuffix.put(suffix, additionalWays);
-
-        return additionalWays + numDec;
-
+        cache[index] = result; // update the cache
+        return result; // return the result
     }
 }
