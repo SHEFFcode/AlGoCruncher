f09760aea014976ad62194901ad9943a1c22e3a2
yuriy-shefer-ck <yuriy.shefer@creditkarma.com>
2019-09-13 23:10:11 -0700

    Get started on verify alien dictionary

---
+++ FaceBook/VerifyAlienDictionary.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        
+    }
+}
+
+/**
+ * Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+ */
