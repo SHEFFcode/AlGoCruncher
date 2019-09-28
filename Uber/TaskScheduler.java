b1bd4d4a1076ce3c9f5016e5f2a1dd7552114d4d
yuriy-shefer-ck <yuriy.shefer@creditkarma.com>
2019-09-27 19:30:01 -0700

    Task scheduler

--- Uber/TaskScheduler.java
+++ Uber/TaskScheduler.java
@@ -1,6 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     /**
      * @param tasks The tasks that need to be processed by the CPU
@@ -8,66 +5,70 @@ class Solution {
      * @return The interval count (including the cool down intervals) to process tasks
      */
     public int leastInterval(char[] tasks, int n) {
-        Map<Character, Integer> charToFrequencyMap = new HashMap<>(tasks.length); // reasonable default
         int cycles = 0;
+        Map<Character, Integer> taskToFrequencyMap = new HashMap<>(tasks.length); // reasonable default
+        PriorityQueue<Integer> mostFrequentTaskQ = new PriorityQueue<>(tasks.length, Collections.reverseOrder()); // we will use a reasonable size and reverse order to get max heap instead of min heap
+
         for (char task : tasks) {
-            charToFrequencyMap.merge(task, 1, Integer::sum);
+            taskToFrequencyMap.merge(task, 1, (oldVal, newVal) -> oldVal + 1);
         }
+        
+        mostFrequentTaskQ.addAll(taskToFrequencyMap.values());
+        
 
-        Set<Character> keys = charToFrequencyMap.keySet();
-        int mapSize = charToFrequencyMap.size();
-
-        while (!(mapSize <= 0)) { // danger zone, make sure we get to empty here
-            for (char key : keys) {
-                if (charToFrequencyMap.get(key) > 0) { // if the key is > 0, we can decrement the occurance count
-                    charToFrequencyMap.merge(key, 0, (oldVal, newVal) -> oldVal - 1);
-                    if (charToFrequencyMap.get(key) == 0) { // if we are at 0, we can reduce the map size
-                        mapSize--;
-                        if (mapSize == 0) {
-                            break;
-                        }
-                        charToFrequencyMap.put(key, -1);
-                    } else { // if we are not at 0, we can increase the # of cycles taken to do the problem.
-                        cycles++;
-                    }
+        while (!mostFrequentTaskQ.isEmpty()) {
+            List<Integer> tasksProcessed = new ArrayList<>(n + 1); // we can have at most n + 1 tasks here
+            for (int i = 0; i < n + 1; i++) { // we should only do n + 1 tasks before doing most frequent task again
+                if (!mostFrequentTaskQ.isEmpty()) { // we can only remove unique tasks till q is empty
+                    tasksProcessed.add(mostFrequentTaskQ.poll()); // let's add processed tasks to our list
                 }
-
             }
 
-            cycles += n;
+            tasksProcessed.forEach(task -> {
+                if (--task > 0) { // we only want unfinished tasks back in the queue
+                    mostFrequentTaskQ.add(task);
+                }
+            });
+
+            // since we are processing these tasks n + 1 at a time (to be most efficient with least non productive idle time)
+            /**
+             * If the q is empty, we will just process however many tasks we have added to the tasks to process list.
+             * If the q is not empty, we will have to wait the full n + 1 cooloff time regardless of how many
+             * items we have been able to process this round.
+             */
+            cycles += mostFrequentTaskQ.isEmpty() ? tasksProcessed.size() : n + 1;
         }
 
-        return cycles;
+        return cycles; // once we are done with the queue, we have finished all the tasks.
     }
 }
 
 /**
  *  Input: tasks = ["A","A","A","B","B","B"], n = 2
-                                         *
     Output: 8
     Explanation: A -> B -> idle -> A -> B -> idle -> A -> B.
 
-    cycles = 0
+    "A" "A" "A" "B" "B" "B"
+     i
 
-    keyToFrequencyMap = {
+    Step 1: Get the frequency map of the all the stasks
+    taskToFrequencyMap = {
         "A": 3,
         "B": 3
     }
 
-    Map.Keys() = ["A", "B"]
+    Step 2: The insight is that the cool down can either be the idle cool down, where we just wait
+    or we can process another task while we are cooling down (productive cool down).
+    So what we will do is create a priority queue, where we will try to process as many tasks
+    as possible from the most frequent task list, so that we can keep the idle period between them
+    as productive as possible.  They can sort of feed the idle period for each other, leaving the
+    least common tasks to the end where they can run once without the non productive cooldown.
+    For this we will use a max heap represented with priority heap in reverse order.
 
-    while (!keyToFrequencyMap.isEmpty()) {
-        keys.forEach(key -> {
-            map.merge(key, 0, (oldVal, newVal) -> oldVal - 1);
-            if (map.get(key) == 0) {
-                map.remove(key); // this will make map empty
-            }
-        })
-        cycles = n + 1;
-    }
+    Note: heap allows duplicate numbers, so we will only put in the integers, without the task names
+    for now.
 
-    return cycles;
-
-    We will now iterate through the keys here, and keep decrementing them till we get to zero0
-
+    Step 3: Loop over the length of n + 1 (which is doing a task and waiting n period cooldown and
+    do that many tasks from the heap, with removing of those tasks from the heap because we can 
+    only take unique tasks from the heap.
  */
