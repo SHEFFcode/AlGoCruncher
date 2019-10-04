1948172b1ce42ed154ada6c88fb4bd8b2325e726
yuriy-shefer-ck <yuriy.shefer@creditkarma.com>
2019-10-03 19:01:41 -0700

    Add more notes and more understanding around the 24 game

--- Uber/24game.java
+++ Uber/24game.java
@@ -1,34 +1,51 @@
 class Solution {
     public boolean judgePoint24(int[] nums) {
-        List allNumbers = new ArrayList<Double>();
+        List<Double> allNumbers = new ArrayList<>();
         for (int v: nums) allNumbers.add((double) v);
         return solve(allNumbers);
     }
-    private boolean solve(ArrayList<Double> nums) {
+    private boolean solve(List<Double> nums) {
         if (nums.size() == 0) return false; // we have used up all the numbers
         if (nums.size() == 1) return Math.abs(nums.get(0) - 24) < 1e-6; // we are down to our last number, let's see if it meets our criteria
 
+        // on the second iteration here, we will see an array of 3 nums instead of 4, where the 3d num is the result of ops on i and j previousely, and other other 2 are the 2 non i and j numbers we picked prior
+        // therefore, here we will conduct our ops between two original numbers and the result of operations on the other 2
+        // later we will go down to 2 numbers, result of two other ops on sums
+        // finally we will get to 1 number, where we will either get a 24 or not. If we get to 0 numbers somehow, let's return false
         for (int i = 0; i < nums.size(); i++) {
             for (int j = 0; j < nums.size(); j++) {
-                if (i != j) {
-                    ArrayList<Double> nums2 = new ArrayList<Double>();
-                    for (int k = 0; k < nums.size(); k++) if (k != i && k != j) {
-                        nums2.add(nums.get(k));
+                if (i != j) { // don't do anything when i and j are the same, we want to pick two diff numbers to work with
+
+                    List<Double> twoNumbers = new ArrayList<>(); // let's make a two number container
+
+
+                    for (int k = 0; k < nums.size(); k++) {
+                        if (k != i && k != j) { // this will initially populate the two numbers that are not at i and j
+                            twoNumbers.add(nums.get(k));
+                        }
                     }
-                    for (int k = 0; k < 4; k++) {
-                        if (k < 2 && j > i) continue;
-                        if (k == 0) nums2.add(nums.get(i) + nums.get(j));
-                        if (k == 1) nums2.add(nums.get(i) * nums.get(j));
-                        if (k == 2) nums2.add(nums.get(i) - nums.get(j));
+
+                    for (int k = 0; k < 4; k++) { // these are the 4 signs
+                        if (k < 2 && j > i) continue; // because there is no difference in commutative operations, let's not double up in our decision space here
+
+                        if (k == 0) twoNumbers.add(nums.get(i) + nums.get(j)); // let's add to the original 2 numbers the result of adding the other 2 numbers numbers at i and j
+                        if (k == 1) twoNumbers.add(nums.get(i) * nums.get(j)); // let's add to the original 2 numbers the result of  multiplying the other 2 numbers at i and j
+
+                        // subtraction is not a commutative operation, so it matters whether we do nums[i] - nums[j] vs nums[j] - nums[i] and nums[i] / nums[j] vs nums[j] / nums[i]
+                        if (k == 2) twoNumbers.add(nums.get(i) - nums.get(j)); //let's add to the original 2 numbers the result of subtracting numbers i and j
+
+                        // below operation I cannot do if j > i without producing a zero which is a useless result
                         if (k == 3) {
-                            if (nums.get(j) != 0) {
-                                nums2.add(nums.get(i) / nums.get(j));
+                            if (nums.get(j) != 0) { // division, so let's be even more careful here
+                                twoNumbers.add(nums.get(i) / nums.get(j));
                             } else {
                                 continue;
                             }
                         }
-                        if (solve(nums2)) return true;
-                        nums2.remove(nums2.size() - 1);
+
+
+                        if (solve(twoNumbers)) return true;
+                        twoNumbers.remove(twoNumbers.size() - 1);
                     }
                 }
             }
