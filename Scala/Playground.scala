ca3ef5960fd5b03df3c65d927642859f38c90ff7
SHEFFmachine <sheff@sheffmachine.com>
2019-10-17 23:05:47 -0700

    Playing around with scala

--- Scala/Playground.scala
+++ Scala/Playground.scala
@@ -8,3 +8,22 @@ val anotherResult = List(1, 2, 3).flatMap(itemFromList1 => List(3, 4, 5).map(ite
 println(result)
 
 println(anotherResult)
+
+def fib(n: Int): Int = {
+  n match {
+    case n if n < 2 => n
+    case _ => fib(n - 1) + fib(n - 2)
+  }
+}
+
+println(fib(1))
+
+case class Person(name: String)
+
+val steve = Person("Peter")
+
+steve match {
+  case Person("Steve") => println("I am steve")
+  case Person("Dave") => println("Dave")
+  case _ => println("I don't know who I am")
+}
