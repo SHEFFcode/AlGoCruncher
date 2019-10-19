59c11b3d401171313012c97540b2c72fc83d9f20
SHEFFmachine <sheff@sheffmachine.com>
2019-10-19 11:18:20 -0700

    Shortest bridge

--- Uber/ShortestBridge.java
+++ Uber/ShortestBridge.java
@@ -1,79 +1,70 @@
+import java.util.ArrayDeque;
+
 class Solution {
-    private int bridgeLength = 0;
-    private int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+    int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };; // faster as q then any other implementation
 
     public int shortestBridge(int[][] A) {
-        int rowCount = A.length, colCount = A[0].length;
+        boolean found = false;
+        int row = A.length, col = A[0].length;
         boolean[][] visited = new boolean[row][col];
-        Queue<Coordinate> islandLands = tabulateIslandLands(A, visited);
 
-        // now let's expand the island in a BFS fashion to find second island
-        while (!islandLands.isEmpty()) {
-            // let's do BFS here
-            int size = islandLands.size();
+        Queue<int[]> q = createFirstIsland(A, visited);
+
+        // BFS - expand the first island, until it reaches the second island
+        int bridgeLength = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
             for (int i = 0; i < size; i++) {
-                Coordinate current = islandLands.poll();
-                if (growIsland(coordinate, A, islandLands, visited)) {
+                int[] cur = q.poll();
+                if (didReachSecondIsland(A, visited, q)) {
                     return bridgeLength;
                 }
             }
             bridgeLength++;
         }
-
-        return 1; // we are guaranteed 2 islands, so the default answer will be 1, we could throw an error also
+        return -1;
     }
 
-    private Queue<Coordinate> tabulateIslandLands(int[][] islandMap, boolean[][] visited) {
-        Queue<Coordinate> islandLands = new ArrayDeque<>(); // ArrayDeque is faster as a queue then linked list
-        boolean found = false;
+    private Queue<int[]> createFirstIsland(int[][] A, boolean[][] visited) {
+        int row = A.length, col = A[0].length;
+        Queue<int[]> q = new ArrayDeque<>();
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (islandMap[i][j] == 1) {
-                    found = true;
-                    getFirstIsland(i, j, islandMap, islands, visited);
-                    return islandLands;
+                if (A[i][j] == 1) {
+                    getFirstIsland(A, i, j, visited, q);
+                    return q;
                 }
             }
         }
-
-        return islandLands;
     }
 
-    private void getFirstIsland(int i, int j, int[][] islandMap, Queue<Coordinate> islandLands, boolean[][] visited) {
-        int row = islandMap.length, col = islandMap[0].length;
-        if (i < 0 || i >= row || j < 0 || j >= col || visited[i][j] == true) {
+    public void getFirstIsland(int[][] A, int i, int j, boolean[][] visited, Queue<int[]> q) {
+        int row = A.length, col = A[0].length;
+        if (i < 0 || i >= row || j < 0 || j >= col || visited[i][j] == true)
             return;
-        }
 
-        islandLands.offer(new Coordinate(i, j)); // let's fill up our queue.
+        q.offer(new int[] { i, j });
         visited[i][j] = true;
+
         for (int[] dir : dirs) {
             int x = i + dir[0], y = j + dir[1];
-            getFirstIsland(x, y, islandMap, islandLands, visited);
+            getFirstIsland(A, x, y, visited);
         }
     }
 
-    private boolean growIsland(Coordinate coordinate, int[][] A, Queue<Coordinate> islandLands, boolean[][] visited) {
+    private boolean didReachSecondIsland(int[][] A, boolean[][] visited, Queue<int[]> q) {
         for (int[] dir : dirs) {
-            int x = coordinate.x + dir[0], y = coordinate.y + dir[1];
-            if (x < 0 || x >= row || y < 0 || y >= col || visited[x][y])
+            int x = cur[0] + dir[0], y = cur[1] + dir[1];
+            if (x < 0 || x >= row || y < 0 || y >= col || visited[x][y]) {
                 continue;
+            }
             if (A[x][y] == 1) {
                 return true;
             }
+
             visited[x][y] = true;
-            islandLands.offer(new Coordinate(x, y));
-        }
-    }
-
-    private class Coordinate {
-        public int x;
-        public int y;
-
-        public Coordinate(int x, int y) {
-            this.x = x;
-            this.y = y;
+            q.offer(new int[] { x, y });
         }
     }
 }
