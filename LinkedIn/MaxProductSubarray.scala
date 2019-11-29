0fdfa312f0f30d9c1935f03dcbc70f7004c455a9
SHEFFmachine <sheff@sheffmachine.com>
2019-11-28 18:24:14 -0800

    Get started on max subarray sum.

---
+++ LinkedIn/MaxProductSubarray.scala
@@ -0,0 +1,11 @@
+object Solution {
+    def maxProduct(nums: Array[Int]): Int = {
+        
+    }
+}
+
+/*
+  Input: [2, 3, -2, 4]
+  Output: 6
+  Explanation: [2, 3] has the largest product 6.
+*/
