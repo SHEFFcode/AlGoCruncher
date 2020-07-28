c1523949c4afa69064b82f6970ccd76fb6f63c16
SHEFFmachine <sheff@sheffmachine.com>
2020-07-27 20:23:22 -0700

    Fizzbuzz

--- Grinder/fizzBuzz.scala
+++ Grinder/fizzBuzz.scala
@@ -1,7 +1,8 @@
 object Solution extends App {
   def fizzBuzz(num: Int) = {
     (1 to num).foreach((n: Int) => {
-      if (n % 3 == 0) println("fizz")
+      if (n % 5 == 0 && n % 3 == 0) println("fizzbuzz")
+      else if (n % 3 == 0) println("fizz")
       else if (n % 5 == 0) println ("buzz")
       else println(n)
     })
