bb2afc0e122cfcd14209b7d2ef978153946565ef
SHEFFmachine <sheff@sheffmachine.com>
2020-07-27 21:47:05 -0700

    Array chunk another way in scala

---
+++ Grinder/anagrams.scala
@@ -0,0 +1,19 @@
+object Solution extends App {
+  def anagrams(strA: String, strB: String): Boolean = {
+    val cleanA = strA.toLowerCase().replace("!", "")
+    val cleanB = strB.toLowerCase().replace("!", "")
+
+    val hashA = cleanA.groupBy(identity)
+    val hashB = cleanB.groupBy(identity)
+
+    hashA == hashB // scala implements == as equals for all u java folks
+  }
+
+  println(anagrams("hi there", "Bye there!"))
+}
+
+/*
+anagrams("rail safety", "fairy tales") => True
+anagrams("RAIL! SAFETY!", "fairy tales") => True
+anagrams("hi there", "Bye there!") => false
+*/
