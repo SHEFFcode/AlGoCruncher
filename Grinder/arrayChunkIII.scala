e63e20b9d0af289e85a8242bb3c76f506a6cbc88
SHEFFmachine <sheff@sheffmachine.com>
2020-07-27 21:30:21 -0700

    Array chunk another way in scala

---
+++ Grinder/arrayChunkIII.scala
@@ -0,0 +1,15 @@
+object Solution extends App {
+  def chunk(arr: Array[Int], chunk: Int) = {
+    val totalChunks = math.ceil(arr.size / chunk.toFloat).toInt // we need to be able to round up here
+    val result = new Array[Array[Int]](totalChunks)
+    (0 until totalChunks).foreach(chunkIndex => {
+      val adjustedIndex = chunkIndex * chunk
+      val currArr = arr.slice(adjustedIndex, adjustedIndex + chunk)
+      
+      result(chunkIndex) = currArr
+    })
+    result
+  }
+
+  println(runtime.ScalaRunTime.stringOf(chunk(Array(1, 2, 3, 4, 5), 2)))
+}
