c2c231c623994afaa4ed53ad7c27eed046c227fd
SHEFFmachine <sheff@sheffmachine.com>
2020-09-07 14:47:40 -0700

    search index

---
+++ FaceBook/removeDuplicates.scala
@@ -0,0 +1,23 @@
+object Solution {
+  def removeDuplicates(nums: Array[Int]): Int = {
+    var ndi = if (nums.length > 0) 1 else 0 // non duplicate index
+
+    // we have to be very careful not to go out of bound here, so since
+    // we will access nums(i + 1) here
+    for (i <- 0 until nums.length - 1) {
+      if (nums(i) != nums(i + 1)) {
+        // we will set the non similar num at the ndi position
+        nums(ndi) = nums(i + 1)
+        ndi += 1 // we will increment ndi, since sacala does not have a ++ operator
+      }
+    }
+
+    ndi
+  }
+}
+
+/*
+ [0, 0, 1, 1, 1, 2, 2, 3, 3, 4],=> 2
+ *
+        #
+ */
