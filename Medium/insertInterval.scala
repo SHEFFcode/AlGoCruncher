dbc00fec533832c82af5b47fb22a181867857bbf
SHEFFmachine <sheff@sheffmachine.com>
2020-10-18 15:24:37 -0700

    Make a clean solution to insert interval

--- Medium/insertInterval.scala
+++ Medium/insertInterval.scala
@@ -5,116 +5,28 @@ object Solution {
       intervals: Array[Array[Int]],
       newInterval: Array[Int]
   ): Array[Array[Int]] = {
-    for (idx <- 0 until intervals.length) {
-      if (newInterval(START_TIME) <= intervals(idx)(END_TIME)) {
-        if (newInterval(START_TIME) >= intervals(idx)(START_TIME)) {
-          // if we find an insertion interval, let's do the insertion
-          val smallerStart =
-            math.min(intervals(idx)(START_TIME), newInterval(START_TIME))
-          val biggestEnd =
-            math.max(intervals(idx)(END_TIME), newInterval(END_TIME))
-          intervals(idx)(START_TIME) = smallerStart // update start time
-          intervals(idx)(END_TIME) = biggestEnd // update end time
-          return mergeIntervals(intervals, idx)
-        } else {
-          // TODO: handle [[1,5]] || [0,0] case
-          if (newInterval(END_TIME) > intervals(idx)(END_TIME)) {
-            // Absorb
-            intervals(idx)(START_TIME) = newInterval(START_TIME)
-            intervals(idx)(END_TIME) = newInterval(END_TIME)
-            return mergeIntervals(intervals, idx)
+    val (acc, last) =
+      intervals.foldLeft((Array.empty[Array[Int]], newInterval)) {
+        case ((result, cInterval), nxtInterval) =>
+          if (cInterval(END_TIME) < nxtInterval(START_TIME)) {
+            // if current interval ends before next one starts, no overlap, append cInterval, consider nextInterval later
+            (result :+ cInterval, nxtInterval)
+          } else if (cInterval(START_TIME) > nxtInterval(END_TIME)) {
+            // if current interval starts after nextInterval ends, no overlap, append next interval, consider cInterval later
+            (result :+ nxtInterval, cInterval)
+          } else {
+            // if there is an overlap, don't append anything, just merge the cInterval and nextInterval into a new super Interval
+            (
+              result,
+              Array(
+                Math
+                  .min(nxtInterval(START_TIME), cInterval(START_TIME)),
+                Math.max(nxtInterval(END_TIME), cInterval(END_TIME))
+              )
+            )
           }
-          return mergeIntervals(newInterval +: intervals, idx)
-        }
       }
-    }
-    // append the new interval
-    intervals :+ newInterval
-  }
-
-  def mergeIntervals(
-      intervals: Array[Array[Int]],
-      idx: Int
-  ): Array[Array[Int]] = {
-    val currentInterval = intervals(idx)
-    for (loc <- (idx + 1) until intervals.length) {
-      val subsequentInterval = intervals(loc)
-      if (shouldMergeIntervals(currentInterval, subsequentInterval)) {
-        // make currentInterval(END_TIME) = subsequentInterval(END_TIME)
-        intervals(idx)(END_TIME) = subsequentInterval(END_TIME)
-        // delete the intervals between them
-        return intervals.slice(0, idx + 1) ++ intervals.slice(
-          loc + 1,
-          intervals.length
-        )
-      }
-    }
-    intervals
-  }
-
-  def shouldMergeIntervals(
-      currentInterval: Array[Int],
-      subsequentInterval: Array[Int]
-  ) = {
-    if (subsequentInterval(START_TIME) == currentInterval(END_TIME)) {
-      true
-    } else if (
-      subsequentInterval(START_TIME) < currentInterval(END_TIME)
-      && subsequentInterval(END_TIME) >= currentInterval(END_TIME)
-    ) {
-      true
-    } else {
-      false
-    }
+    // this just takes into account the last left over item
+    acc :+ last
   }
 }
-
-/*
-  G: nonOverlappingIntervals: Array[Array[Int]], newInterval: Array[Int]
-  O: newNonOverlappingIntervals: Array[Array[Int]]
-  T: Any
-  S: Any
-
-  Notes:
-    - Each interval is a tuple represented by Array[Int] of size 2
-    - Intervals are sorted by end time in ascending order
-    - New interval will also be a tuple represented by Array[Int] of size 2
-
-
-Ex 1:
-[[1,3],[6,9]], [2,5] => [[1,5],[6,9]]
-
-[ [ 1 , 5 ] , [ 6 , 9 ] ]   [ 2, 5 ]
- *
-                              #
-
-Ex2:
-[[1,2],[3,5],[6,7],[8,10],[12,16]] || [4,8] => [[1,2],[3,10],[12,16]]
-
-[[1,2],[3,8],[6,7],[7,10],[12,16]]
-
-
-Ex3:
-[[1,5]] || [0,0] => [[0,0],[1,5]]
-
-Ex4:
-
-[[1,5]] || [0,6] => [[0,6],[1,5]]
-
-
-
-Steps:
-1) Find an interval where newInterval[START_TIME] <= existingInterval[END_TIME]
-  - If we cannot find just an interval, just append the interval to the end of the array
-  if (newInterval[START_TIME] >= existingInterval[START_TIME]) do below,
-  If not, prepend or absorb
-2) Modify the end time and start time of the existing interval as follows:
-  - Make currentInterval[END_TIME] = newInterval[END_TIME]
-3) Merge Intervals:
-  - If the subsequentInterval(START_TIME) == currentInterval(END_TIME):
-      # delete intervals between them
-      # make currentInterval(END_TIME) = subsequentInterval(END_TIME)
-  - If subsequentInterval(START_TIME) < currentInterval(END_TIME) && subsequentInterval(END_TIME) > currentInterval(END_TIME)
-      # delete the intervals between them
-      # make currentInterval(END_TIME) = subsequentInterval(END_TIME)
- */
