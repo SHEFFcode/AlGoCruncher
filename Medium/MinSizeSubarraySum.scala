1577542cd4800f5eff27451342b18fb17d2dd872
SHEFFmachine <sheff@sheffmachine.com>
2020-11-15 13:35:33 -0800

    Code sample with index computation

---
+++ Medium/MinSizeSubarraySum.scala
@@ -0,0 +1,18 @@
+object Solution {
+  def minSubArrayLen(s: Int, nums: Array[Int]): Int = {}
+}
+
+/*
+G: s: Int, nums: Array[Int]
+O: minSizeSubarraySum: Int
+T: O(N) || O(n log(n))
+S: Any
+
+Notes:
+  - nums will be positive integers
+  - s will be a positive integer
+  - Subarray will need to be contiguous
+  - sum >= s
+  - If non found, return 0 instead
+  - We are looking for the minimal length of such array
+ */
