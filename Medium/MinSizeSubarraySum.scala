7ac9e7bb572b8c06eb8f871dab2683e39f1c80ee
SHEFFmachine <sheff@sheffmachine.com>
2020-11-16 08:11:23 -0800

    Min size subarray sum

--- Medium/MinSizeSubarraySum.scala
+++ Medium/MinSizeSubarraySum.scala
@@ -1,5 +1,25 @@
-object Solution {
-  def minSubArrayLen(s: Int, nums: Array[Int]): Int = {}
+object Solution extends App {
+  def minSubArrayLen(s: Int, nums: Array[Int]): Int = {
+    var minSubarrLen = nums.length + 1 // Reasonable max
+    var sumSoFar = 0
+    var left = 0
+
+    // grow the window
+    for (right <- 0 until nums.length) {
+      sumSoFar += nums(right)
+      // shrink the window
+      while (sumSoFar >= s) {
+        // +1 here to adjust for 0 based counting
+        minSubarrLen = minSubarrLen min (right + 1 - left)
+        sumSoFar -= nums(left) // shrink sum
+        left += 1 // shrink window
+      }
+    }
+
+    if (minSubarrLen < nums.length + 1) minSubarrLen else 0
+  }
+
+  println(minSubArrayLen(7, Array(2, 3, 1, 2, 4, 5)))
 }
 
 /*
