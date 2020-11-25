cf292cbd1eeaef180c7adbeaeb48eaa5ff2cf910
SHEFFmachine <sheff@sheffmachine.com>
2020-11-24 20:07:41 -0800

    Min cost to hire k workers.

---
+++ GO_ogle/minCostToHireKWorkers.scala
@@ -0,0 +1,77 @@
+import scala.collection.mutable.PriorityQueue
+object Solution extends App {
+  def mincostToHireWorkers(
+      quality: Array[Int],
+      wage: Array[Int],
+      K: Int
+  ): Double = {
+    val len = quality.length
+    val workers = wage
+      .zip(quality)
+      .sortWith((a, b) =>
+        a._1.toDouble / a._2.toDouble < b._1.toDouble / b._2.toDouble
+      ) // sorted by wage to quality
+
+    val wageToQuality = workers.map((a) => a._1.toDouble / a._2.toDouble)
+    println(scala.runtime.ScalaRunTime.stringOf(workers))
+    println(scala.runtime.ScalaRunTime.stringOf(wageToQuality))
+    var minCostForK = Double.MaxValue
+    var sumQualities = 0
+
+    val pq = PriorityQueue[Integer]()
+    for (worker <- workers) {
+      pq.enqueue(worker._2)
+      sumQualities += worker._2
+      println(scala.runtime.ScalaRunTime.stringOf(pq))
+      if (pq.size > K) {
+        sumQualities -= pq.dequeue()
+      }
+      if (pq.size == K) {
+        // since we have already sorted the workers, we know this workers ratio is highest
+        // of the ones currently in the queue, and we can apply this foruma
+        minCostForK =
+          math.min(minCostForK, sumQualities.toDouble * worker._1 / worker._2)
+        println(sumQualities)
+      }
+    }
+
+    minCostForK
+  }
+
+  println(mincostToHireWorkers(Array(10, 20, 5), Array(70, 50, 30), 2))
+}
+
+/*
+G: quality: Array[Int], wage: Array[Int], K: Int
+O: minWageExpense: Double
+T: Any
+S: Any
+
+Notes:
+  - K represents the number of workers to be hired
+  - Conditions for hiring:
+    ^ Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
+    ^ Every worker in the paid group must be paid at least their minimum wage expectation.
+
+Ex:
+
+Input: quality = [10,20,5], wage = [70,50,30], K = 2
+Output: 105.00000
+Explanation: We pay 70 to 0-th worker and 35 to 2-th worker.
+
+[10,20,5] | [70,50,30] | 2
+
+wageToQuality: [7, 2.5, 6]
+
+
+Ex2:
+
+Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], K = 3
+Output: 30.66667
+Explanation: We pay 4 to 0-th worker, 13.33333 to 2-th and 3-th workers seperately.
+
+[3,1,10,10,1] | [4,8,2,2,7] | k = 3
+
+[12, 8, 20, 20, 7]
+
+ */
