10f881f1b6c1a8951da6ffdc8a0200bdc02f5923
SHEFFmachine <sheff@sheffmachine.com>
2020-11-28 12:35:47 -0800

    Flatten tree using morris traversal

---
+++ FaceBook/flattenTreeUsingMorrisTraversal.scala
@@ -0,0 +1,66 @@
+/**
+  * Definition for a binary tree node.
+  * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+  *   var value: Int = _value
+  *   var left: TreeNode = _left
+  *   var right: TreeNode = _right
+  * }
+  */
+object Solution {
+  def flatten(root: TreeNode): Unit = {
+    if (root == null) return // easy part
+    var cNode = root
+    while (cNode != null) {
+      if (cNode.left != null) {
+        // if we have a left child
+        // find it's rightmost node (rightMost child of cNode)
+        var rightMost = cNode.left
+        while (rightMost.right != null) {
+          rightMost = rightMost.right
+        }
+
+        // rewrite the connections
+
+        // regardless of existence of left node
+        // rewire the connections
+        rightMost.right = cNode.right
+        cNode.right = cNode.left
+        cNode.left = null
+      }
+      // move on to the right side of the tree
+      cNode = cNode.right
+    }
+  }
+}
+
+/*
+G: root: TreeNode
+O: flattenedRoot: TreeNode
+T: Any
+S: Any
+
+Notes:
+  - Just need to be a single list heading right
+
+Ex:
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+
+=>
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+
+ */
