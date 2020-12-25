22db09782eae03054fea63a738102014b7484a99
SHEFFmachine <sheff@sheffmachine.com>
2020-12-25 13:26:36 -0800

    Word squares with more info on runtime

--- GO_ogle/wordSquares.scala
+++ GO_ogle/wordSquares.scala
@@ -53,11 +53,35 @@ object Solution {
 /*
 G: words: Array[String]
 O: allWordSquares: List[List[String]]
-T:
-S:
+T: O(N⋅26^L) where L is the length of a single word
+S: O(N⋅L) where L is the length of a single word
 
 Notes:
+  - Given a set of words (without duplicates)
+  - There are at least 1 and at most 1000 words.
+  - All words will have the exact same length.
+  - Word length is at least 1 and at most 5.
+  - Each word contains only lowercase English alphabet a-z.
 
 Ex:
+Input:
+["area","lead","wall","lady","ball"]
+
+Output:
+[
+  [ "wall",
+    "area",
+    "lead",
+    "lady"
+  ],
+  [ "ball",
+    "area",
+    "lead",
+    "lady"
+  ]
+]
+
+Explanation:
+The output consists of two word squares. The order of output does not matter (just the order of words in each word square matters).
 
  */
