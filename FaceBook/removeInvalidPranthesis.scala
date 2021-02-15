c4b47468f74bcf02ad58bc0ba48fb5f0b0c8f365
SHEFFmachine <sheff@sheffmachine.com>
2021-02-15 09:45:24 -0800

    remove invalid paranthesis with explanation

--- FaceBook/removeInvalidPranthesis.scala
+++ FaceBook/removeInvalidPranthesis.scala
@@ -5,6 +5,7 @@ object Solution {
     ogString = s
     ans = List[String]()
 
+    // Step 1: Calculate left and right extras
     val (lExtra, rExtra) = s.foldLeft((0, 0)) {
       case ((lExtra, rExtra), char) =>
         char match {
@@ -17,9 +18,10 @@ object Solution {
         }
     }
 
-    println(lExtra, rExtra)
-
+    // println(lExtra, rExtra)
+    // Create a list of possible answers
     dfs(0, "", 0, lExtra, rExtra)
+    // Return distinct answers
     ans.distinct
   }
 
@@ -30,25 +32,37 @@ object Solution {
       lExtra: Int,
       rExtra: Int
   ): Unit = {
+    // If we have reached the length of original string
     if (idx >= ogString.length()) {
-      if (lNoMatch == 0) ans ::= cStr
+      // And we don't have any unmatched left open brackets
+      if (lNoMatch == 0) ans ::= cStr // add to results
       return
     }
 
+    // match a char
     ogString(idx) match {
       case c @ '(' =>
         if (lExtra > 0) {
+          // If we have some lExtras to get rid of, do it
+          // without adding any chars to the cString
           dfs(idx + 1, cStr, lNoMatch, lExtra - 1, rExtra)
         }
+        // ALSO look if you can count this as yet to be matched
+        // opening brace, while adding to the cString
         dfs(idx + 1, cStr + c, lNoMatch + 1, lExtra, rExtra)
       case c @ ')' =>
         if (rExtra > 0) {
+          // IF we have some rExtras, reduce the count
+          // without adding to the cString
           dfs(idx + 1, cStr, lNoMatch, lExtra, rExtra - 1)
         }
         if (lNoMatch > 0) {
+          // If there are lNoMatches, reduce them by adding to the
+          // cString to close out the unmatched opening brace
           dfs(idx + 1, cStr + c, lNoMatch - 1, lExtra, rExtra)
         }
       case c @ _ =>
+        // Just add to the cString as is.
         dfs(idx + 1, cStr + c, lNoMatch, lExtra, rExtra)
     }
   }
