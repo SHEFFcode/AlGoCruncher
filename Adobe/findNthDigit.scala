f47086691fd8e57af76a334b356b37ff45b8cae6
SHEFFmachine <sheff@sheffmachine.com>
2021-05-31 18:35:41 -0500

    Try to better understand the nth digit, but it's still hard to figure out

--- Adobe/findNthDigit.scala
+++ Adobe/findNthDigit.scala
@@ -1,26 +1,26 @@
 object Solution {
   def findNthDigit(n: Int): Int = {
-    var digits = 1
-    var base: Long = 9
-    var input: Long = n
-    var levelBound: Long = 0
+    var digitLvl = 1 // 1 for 0 -> 9, 2 for 9 -> 99 etc
+    var base: Long = 9 // base starts at 9, used 4 count
+    var overLvlBound: Long = n
+    var levelBound: Long = 0 // the 9, 99 from above
     // find digit
-    while (input > base * digits) {
-      input -= base * digits
-      base = base * 10
-      levelBound = levelBound * 10 + 9
-      digits += 1
+    while (overLvlBound > base * digitLvl) {
+      overLvlBound -= base * digitLvl // 9, 90, 900 etc
+      base = base * 10 // go up by 10, so 9, 99, 999 etc
+      levelBound = levelBound * 10 + 9 // see above
+      digitLvl += 1
     } //O(1)
 
-    println(s"Deduced digit level $digits, last boundary number: $levelBound")
-    var nthDigit = input % digits
+    println(s"Deduced digit level $digitLvl, last boundary number: $levelBound")
+    var nthDigit = // since remainder of 0 means we are exactly divide into n segs
+      if (overLvlBound % digitLvl == 0) digitLvl else overLvlBound % digitLvl
 
     val number: Long = if (nthDigit == 0) {
-      nthDigit = digits
-      input / digits + levelBound
-      //this will give you the number at this digit level e.g. 11-9 =2 /2 = 1 => this is the first number in 2-digit numbers, which is 10
+      overLvlBound / digitLvl + levelBound
+      //this will give you the number at this digit level e.g. 11-9 =2 / 2 = 1 => this is the first number in 2-digit numbers, which is 10
     } else {
-      input / digits + 1 + levelBound
+      overLvlBound / digitLvl + 1 + levelBound
     }
 
     println(s"Deduced number: $number, need $nthDigit th digit")
